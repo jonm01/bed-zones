cc9c9c2cfbeb8332a1813c945cdcd3e36b07c1cc
Jon Mathew <16651693+jonm01@users.noreply.github.com>
2025-08-22 11:40:33 +0200

    Add dual-zone bed UI component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import BedDualZone from "@/components/BedDualZone";
 
 export default function Home() {
   return (
@@ -19,6 +20,13 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+
+        <BedDualZone
+          leftStatus="heating"
+          rightStatus="cooling"
+          selectedSide="left"
+          editingSide="right"
+        />
 
         <div className={styles.ctas}>
           <a
